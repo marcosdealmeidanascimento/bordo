9d472010acaf407a531072a77afebd3411643a63
marcosdealmeidanascimento <marcos.800nascimento@gmail.com>
2023-08-25 13:26:13 -0300

    feat: :sparkles: Criando a tabela atividade

--- backend/app/db/migrations/versions/1812da51ff78_create_activity_table.py
+++ backend/app/db/migrations/versions/1812da51ff78_create_activity_table.py
@@ -7,6 +7,7 @@ Create Date: 2023-08-25 16:07:32.957847
 """
 from alembic import op
 import sqlalchemy as sa
+from sqlalchemy.dialects import postgresql
 
 
 # revision identifiers, used by Alembic.
@@ -17,8 +18,24 @@ depends_on = None
 
 
 def upgrade() -> None:
-    pass
+    op.create_table(
+        "activity",
+        sa.Column("id", sa.Integer, nullable=False),
+        sa.Column("description", sa.Text(), nullable=False),
+        sa.Column("start_time", sa.Time(), nullable=False),
+        sa.Column("category_id", sa.Integer, nullable=False),
+        sa.Column("completion_time", sa.Time(), nullable=False),
+        sa.Column("user_id", postgresql.UUID(as_uuid=True), nullable=False),
+        sa.Column("logbook_id", sa.Integer, nullable=False),
+        sa.Column("created_at", sa.DateTime(), nullable=True),
+        sa.Column("updated_at", sa.DateTime(), nullable=True),
+        sa.PrimaryKeyConstraint("id"),
+        sa.ForeignKeyConstraint(["user_id"], ["user.id"],),
+        sa.ForeignKeyConstraint(["logbook_id"], ["logbook.id"],),
+        sa.ForeignKeyConstraint(["category_id"], ["category.id"],),
+    )
 
 
 def downgrade() -> None:
-    pass
+    op.drop_table("activity")
+
