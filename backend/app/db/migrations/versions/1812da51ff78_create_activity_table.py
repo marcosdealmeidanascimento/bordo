d362cebf51efd6a122e52c03d3a42c4c98eb394c
marcosdealmeidanascimento <marcos.800nascimento@gmail.com>
2023-08-25 13:07:54 -0300

    Criando tabela atividade

---
+++ backend/app/db/migrations/versions/1812da51ff78_create_activity_table.py
@@ -0,0 +1,24 @@
+"""create activity table
+
+Revision ID: 1812da51ff78
+Revises: f8aef32a4435
+Create Date: 2023-08-25 16:07:32.957847
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '1812da51ff78'
+down_revision = 'f8aef32a4435'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
