3776c1b5d8e0c675db8650843b45a3bda5e93086
marcosdealmeidanascimento <marcos.800nascimento@gmail.com>
2023-09-10 13:01:55 -0300

    fix: :bug: Remove seld

--- backend/app/db/repositories/crud_post.py
+++ backend/app/db/repositories/crud_post.py
@@ -22,7 +22,7 @@ class CRUDPost(CRUDBase[Post, PostCreate, PostUpdate]):
         await db.refresh(db_obj)
         return db_obj
     
-    async def update(seld, db: AsyncSession, *, db_obj: Post, obj_in: Union[PostUpdate, Dict[str, any]]) -> Post:
+    async def update(self, db: AsyncSession, *, db_obj: Post, obj_in: Union[PostUpdate, Dict[str, any]]) -> Post:
         if isinstance(obj_in, dict):
             update_data = obj_in
         else:
