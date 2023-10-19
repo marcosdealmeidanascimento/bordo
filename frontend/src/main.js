fdf3102e9e6ea4a6fe34199549bb321bbbe0b52a
marcosdealmeidanascimento <marcos.800nascimento@gmail.com>
2023-10-19 14:03:37 -0300

    fix: :bug: Refresh page perdendo o theme

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -8,8 +8,7 @@ import { createPinia } from 'pinia'
 import 'primevue/resources/primevue.min.css';
 import 'primeicons/primeicons.css';
 import 'primeflex/primeflex.css';
-// import 'primevue/resources/themes/tailwind-light/theme.css';
-// import 'primevue/resources/themes/nano/theme.css';
+import 'primevue/resources/themes/saga-blue/theme.css';
 import { createI18n } from 'vue-i18n'
 import messages from './helpers/locale/message.js'
 import router from './router'
@@ -22,8 +21,6 @@ import Dialog from 'primevue/dialog';
 import ConfirmDialog from 'primevue/confirmdialog';
 import ConfirmationService from 'primevue/confirmationservice';
 import ToastService from 'primevue/toastservice';
-let themeElement = document.getElementById('theme-link')
-themeElement.setAttribute('href', 'node_modules/primevue/resources/themes/saga-blue/theme.css');
 
 const pinia = createPinia()
 const app = createApp(App);
